aa64442d2292716bf3a51c295e42894f8feec0ea
manishkasala <51794891+manishkasala@users.noreply.github.com>
2021-08-25 21:57:51 +0530

    day2

---
+++ day2.js
@@ -0,0 +1,19 @@
+//creating a array of objects with food data with properties (name,price,quantity)
+//and display it using a for loop in the console
+let food=[
+    {name:"pasta",
+    price:100,
+    quantity:1
+    },
+    {name:"burger",
+    price:120,
+    quantity:6
+    },
+    {name:"pizza",
+    price:180,
+    quantity:7
+    }
+]
+for(let i=0;i<food.length;i++){
+    console.log(food[i]);
+}
